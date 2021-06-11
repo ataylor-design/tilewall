47b810c0a66e8411c6e67202fb7c738a9725e7b0
Antonia Taylor <45052816+atayroller@users.noreply.github.com>
2021-06-11 11:30:48 -0700

    misc things

--- image.php
+++ image.php
@@ -4,6 +4,7 @@
 		<title>Tilewall</title>
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="resources/css/global.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/image.css">
 	</head>
 	<body>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<title>Tilewall</title>
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="resources/css/global.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/index.css">
 	</head>
 	<body>
@@ -12,7 +13,7 @@
 		</main>
 		<footer>
 			<p>Tilewall is created by <a href="https://antoniataylordesign.com/">Antonia Taylor, based off imagedumper created by <a href="https://heckscaper.com/">Emma Essex</a></p>
-			<p>Tilewall is licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a></p>
+			<p>Tilewall is licensed under MIT.</p>
 		</footer>
 	</body>
 </html>
